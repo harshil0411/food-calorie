f3adb2f0fe629abb6e07c63f379bce7835c33beb
harshil0411 <mehtaharshil2004@gmail.com>
2025-03-11 15:45:23 +0530

    initial

--- counter/views.py
+++ counter/views.py
@@ -7,15 +7,15 @@ def home(request):
     import requests
     if request.method == 'POST':
         query = request.POST['query']
-        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
         api_request = requests.get(
-            api_url + query, headers={'X-Api-Key': 'YOUR-API-KEY'})
+            api_url + query, headers={'X-Api-Key': 'spdwxHwE7hDs4xVR/eSVPQ==zYNauQEAY39rmqro'})
         try:
             api = json.loads(api_request.content)
-            print(api_request.content)
+            print(api["items"], "=======")
         except Exception as e:
             api = "oops! There was an error"
             print(e)
-        return render(request, 'home.html', {'api': api})
+        return render(request, 'home.html', {'api': api["items"]})
     else:
         return render(request, 'home.html', {'query': 'Enter a valid query'})
